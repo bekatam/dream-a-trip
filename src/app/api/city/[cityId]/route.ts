2ae370cf6b20ebec068f5e4e319dc3f9683c573d
bekatam <109537301+bekatam@users.noreply.github.com>
2023-07-21 00:04:29 +0600

    Update route.ts

--- src/app/api/city/[cityId]/route.ts
+++ src/app/api/city/[cityId]/route.ts
@@ -21,18 +21,14 @@ export async function DELETE(req: NextRequest, res: NextResponse) {
 	try {
 		await connectToMongo();
 
-		// Find the city with the given ID
 		const city = await cityModel.findById(id);
 
-		// Find the destination index based on the destination ID
 		const destinationIndex = city.destinations.findIndex(
 			(destination: any) => destination._id.toString() === id
 		);
 
-		// Remove the destination from the destinations array
 		city.destinations.splice(destinationIndex, 1);
 
-		// Save the updated city
 		await city.save();
 
 		return new Response("ok");
