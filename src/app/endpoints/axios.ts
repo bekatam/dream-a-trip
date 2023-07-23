a3483be61016541669714c8d6719760634cd46bc
bekatam <1bekatam@gmail.com>
2023-07-23 16:07:53 +0600

    added getdata

--- src/app/endpoints/axios.ts
+++ src/app/endpoints/axios.ts
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 export const getData = async () => {
 	const res = await axios
-		.get("http://localhost:3000/api/city")
+		.get("http://dream-a-trip.vercel.app/api/city")
 		.then((data: any) => data.data);
 	return res;
 };
