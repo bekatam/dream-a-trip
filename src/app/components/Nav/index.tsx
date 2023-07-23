6428fdc2e9d18fd1df4ce742a640ae4c832e6d19
bekatam <1bekatam@gmail.com>
2023-07-23 13:00:50 +0600

    added coming soon disclaimer

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -1,18 +1,22 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Nav = () => {
-  return (
-    <div className='w-full flex px-20 justify-between items-center pb-1 pt-1 bg-white'>
-        <Link href='/' className='text-xl'>
-            <Image src={require('@assets/logo.png')} alt='logo' height={48}/>
-        </Link>
-        <Link href='/' className='text-xl'>Map</Link>
-        <Link href='/list' className='text-xl'>List</Link>
-        <button className="text-xl">Sign In</button>
-    </div>
-  )
-}
+	return (
+		<div className="w-full flex px-20 justify-between items-center pb-1 pt-1 bg-white">
+			<Link href="/" className="text-xl">
+				<Image src={require("@assets/logo.png")} alt="logo" height={48} />
+			</Link>
+			<Link href="/" className="text-xl">
+				Map
+			</Link>
+			<Link href="/list" className="text-xl">
+				List
+			</Link>
+			<button className="text-xl">Sign In (Coming Soon)</button>
+		</div>
+	);
+};
 
-export default Nav
+export default Nav;
