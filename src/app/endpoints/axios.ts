a5b6c9fb9b4c5b3eca92fe68e9857b4dd6109544
bekatam <1bekatam@gmail.com>
2023-07-23 16:14:55 +0600

    edit get

--- src/app/endpoints/axios.ts
+++ src/app/endpoints/axios.ts
@@ -1,8 +1,6 @@
 const axios = require("axios");
 
 export const getData = async () => {
-	const res = await axios
-		.get("http://dream-a-trip.vercel.app/api/city")
-		.then((data: any) => data.data);
+	const res = await axios.get("/api/city").then((data: any) => data.data);
 	return res;
 };
--- src/app/utils/connectMongo.ts
+++ src/app/utils/connectMongo.ts
@@ -4,7 +4,6 @@ dotenv.config();
 
 const connectToMongo = async () => {
 	const MONGO_URL = await process.env.MONGO_URL;
-	console.log(MONGO_URL);
 	await mongoose.connect(
 		MONGO_URL,
 		{ useNewUrlParser: true },
