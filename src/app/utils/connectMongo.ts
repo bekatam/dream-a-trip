c44131b65e3db431457ac4203d3625b187dd4d92
bekatam <1bekatam@gmail.com>
2023-07-23 16:05:41 +0600

    added await

--- src/app/utils/connectMongo.ts
+++ src/app/utils/connectMongo.ts
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const MONGO_URL = process.env.MONGO_URL;
 
 const connectToMongo = async () => {
+	const MONGO_URL = await process.env.MONGO_URL;
+	console.log(MONGO_URL);
 	await mongoose.connect(
 		MONGO_URL,
 		{ useNewUrlParser: true },
