ffad132c4159ea35ddd596bc7311d97e9a27deb6
bekatam <1bekatam@gmail.com>
2023-11-03 00:44:55 +0600

    added button click logic

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -8,27 +8,43 @@ const poppins = Poppins({ subsets: ['latin'], weight: '500' })
 
 const Signin = () => {
   const [emailValue, setEmailValue] = useState('');
-  const [emailHint, setEmailHint] = useState(false);
+  const [emailHint, setEmailHint] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
-  const [passwordHint, setPasswordHint] = useState(false);
+  const [passwordHint, setPasswordHint] = useState('');
   const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
   const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
   useEffect(()=>{
     if(emailValue && !emailRegex.test(emailValue)) {
-      setEmailHint(true)
+      setEmailHint('E-mail должен быть в формате example@example.com')
     }
     else {
-      setEmailHint(false)
+      setEmailHint('')
     }
   },[emailValue])
   useEffect(()=>{
     if(passwordValue && !passRegex.test(passwordValue)) {
-      setPasswordHint(true)
+      setPasswordHint('Пароль не соответсвует минимальным требованиям')
     }
     else {
-      setPasswordHint(false)
+      setPasswordHint('')
     }
   },[passwordValue])
+  
+  const validateData = () => {
+    const condition = !passwordHint && !emailHint && emailValue && passwordValue
+    if(condition) {
+      // fetch data
+    }
+    else {
+      if(!emailValue) {
+        setEmailHint('Заполните данное поле')
+      }
+      if(!passwordValue) {
+        setPasswordHint('Заполните данное поле')
+      }
+    }
+  }
+
   return (
     <>
     <div className='w-full signin'>
@@ -41,14 +57,14 @@ const Signin = () => {
         <h3 className={poppins.className + ' text-black my-4 text-center text-3xl'}>LOG IN</h3>
         <label htmlFor='email'>E-mail</label>
         <input type='email' onChange={(e)=>setEmailValue(e.target.value)} id='email' className='rounded-md p-1'/>
-        <p className={(emailHint ? 'opacity-100' : 'opacity-0') + ' hint'}>E-mail должен быть в формате example@example.com</p>
+        <p className={(emailHint ? 'opacity-100' : 'opacity-0') + ' hint'}>{emailHint}</p>
         <label className='mt-1' htmlFor='password'>Password</label>
         <input type='password' onChange={(e)=>setPasswordValue(e.target.value)} id='password' className='rounded-md p-1'/>
         <div className="password">
-          <p className={(passwordHint ? 'opacity-100' : 'opacity-0') + ' hint hint_password'}>Пароль не соответсвует минимальным требованиям</p>
+          <p className={(passwordHint ? 'opacity-100' : 'opacity-0') + ' hint hint_password'}>{passwordHint}</p>
           <button className='rounded-xl ml-auto btn'>Forgot password?</button>
         </div>
-        <button className='rounded-xl mx-auto mt-5 bg-orange-300 w-fit px-5 py-1'>Login</button>
+        <button className='rounded-xl mx-auto mt-5 bg-orange-300 w-fit px-5 py-1' onClick={validateData}>Login</button>
         <button className='rounded-xl mx-auto text-xs btn'>{"Don't"} have account yet?</button>
       </div>
     </div>
