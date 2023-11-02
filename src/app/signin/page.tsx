11a457b9c653fe9bbc7031777b40312b806fbbd3
bekatam <1bekatam@gmail.com>
2023-11-03 00:10:38 +0600

    added email and password validation and css classes

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import "./Signin.css";
 import Image from "next/image";
 import { Poppins } from 'next/font/google';
@@ -6,24 +7,50 @@ import { Poppins } from 'next/font/google';
 const poppins = Poppins({ subsets: ['latin'], weight: '500' })
 
 const Signin = () => {
+  const [emailValue, setEmailValue] = useState('');
+  const [emailHint, setEmailHint] = useState(false);
+  const [passwordValue, setPasswordValue] = useState('');
+  const [passwordHint, setPasswordHint] = useState(false);
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+  useEffect(()=>{
+    if(emailValue && !emailRegex.test(emailValue)) {
+      setEmailHint(true)
+    }
+    else {
+      setEmailHint(false)
+    }
+  },[emailValue])
+  useEffect(()=>{
+    if(passwordValue && !passRegex.test(passwordValue)) {
+      setPasswordHint(true)
+    }
+    else {
+      setPasswordHint(false)
+    }
+  },[passwordValue])
   return (
     <>
     <div className='w-full signin'>
     </div>
     <div className='absolute top-1/2 w-full -translate-y-1/2'>
-      <form className='flex flex-col mx-auto gap-1'>
+      <div className='form flex flex-col mx-auto'>
         <div className='image__wrapper'>
           <Image src={require("@assets/logo.png")} className='logo_image' alt="logo"/>
         </div>
-        <h3 className={poppins.className + ' text-black my-5 text-center text-3xl'}>LOG IN</h3>
+        <h3 className={poppins.className + ' text-black my-4 text-center text-3xl'}>LOG IN</h3>
         <label htmlFor='email'>E-mail</label>
-        <input type='email' id='email' className='rounded-md p-1'/>
-        <label className='mt-3' htmlFor='password'>Password</label>
-        <input type='password' id='password' className='rounded-md p-1'/>
-        <button className='rounded-xl ml-auto text-sm'>Forgot password?</button>
+        <input type='email' onChange={(e)=>setEmailValue(e.target.value)} id='email' className='rounded-md p-1'/>
+        <p className={(emailHint ? 'opacity-100' : 'opacity-0') + ' email_hint'}>E-mail должен быть в формате example@example.com</p>
+        <label className='mt-1' htmlFor='password'>Password</label>
+        <input type='password' onChange={(e)=>setPasswordValue(e.target.value)} id='password' className='rounded-md p-1'/>
+        <div className="password w-full justify-between">
+          <p className={(passwordHint ? 'opacity-100' : 'opacity-0') + ' email_hint'}>Пароль должен содержать 8 символов, заглавные и маленькие буквы, а также специальные символы</p>
+          <button className='rounded-xl ml-auto text-sm'>Forgot password?</button>
+        </div>
         <button className='rounded-xl mx-auto mt-5 bg-orange-300 w-fit px-5 py-1'>Login</button>
         <button className='rounded-xl mx-auto text-xs'>{"Don't"} have account yet?</button>
-      </form>
+      </div>
     </div>
     </>
   )
